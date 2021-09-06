85cd5c4861655074a5d4db30ef8ca95f2235365d
Girish21 <girishkumarv21@gmail.com>
2021-09-06 22:36:05 +0530

    more variations

--- simple_enum/src/main.rs
+++ simple_enum/src/main.rs
@@ -4,9 +4,12 @@ fn main() {
     let v2 = Kind::V2(String::from("Hello, Rust!"));
     let v3 = Kind::V3(1, 1, -1);
 
+    let m1 = Message::Write(String::from("Hello"));
+
     print(v1);
     print(v2);
-    print(v3)
+    print(v3);
+    print_message(m1);
 }
 
 enum Kind {
@@ -15,10 +18,26 @@ enum Kind {
     V3(u32, u32, i32),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn print(kind: Kind) {
     match kind {
         Kind::V1(x, y) => println!("Variant 1: {}, {}", x, y),
         Kind::V2(x) => println!("Variant 2: {}", x),
         Kind::V3(x, y, z) => println!("Variant 3: {}, {}, {}", x, y, z),
+    }
+}
+
+fn print_message(message: Message) {
+    match message {
+        Message::ChangeColor(r, g, b) => println!("new Color: RGB({}, {}, {})", r, g, b),
+        Message::Move { x, y } => println!("Move to: ({}, {})", x, y),
+        Message::Quit => println!("Quit"),
+        Message::Write(val) => println!("Message: {}", val),
     }
 }
