ab9685a0d230ed6bb344e8b43f5fe1b45a00b8db
Girish21 <girishkumarv21@gmail.com>
2021-09-07 23:04:04 +0530

    some more vec

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -1,21 +1,33 @@
-enum VecType {
-    Int(i32),
-    Text(String),
-    Float(f32),
-}
+#![allow(dead_code, unused_variables)]
 
 fn main() {
-    let mut v: Vec<VecType> = Vec::new();
+    let a: [i32; 3] = [1, 2, 3];
+    let mut arr: Vec<VecType> = Vec::new();
 
-    v.push(VecType::Text(String::from("Hello")));
-    v.push(VecType::Int(10));
-    v.push(VecType::Float(10.0));
+    arr.push(VecType::V2(10));
+    arr.push(VecType::V3(10.5));
+    arr.push(VecType::V1(String::from("Hello Vector")));
 
-    for el in &v {
-        match el {
-            VecType::Text(t) => println!("Type text: {}", t),
-            VecType::Int(t) => println!("Type Int: {}", t),
-            VecType::Float(t) => println!("Type Float: {}", t),
+    // let copy = &arr;
+    let mut_copy = &mut arr;
+    mut_copy.push(VecType::V1(String::from("Pushed via borrow")));
+    push(mut_copy);
+
+    for elem in mut_copy.iter() {
+        match elem {
+            VecType::V1(x) => println!("It's a string: {}", x),
+            VecType::V2(x) => println!("It's a number: {}", x),
+            VecType::V3(x) => println!("It's a floating point number: {}", x),
         }
     }
+}
+
+fn push(vec: &mut Vec<VecType>) {
+    vec.push(VecType::V1(String::from("from function")));
+}
+
+enum VecType {
+    V1(String),
+    V2(i32),
+    V3(f32),
 }
